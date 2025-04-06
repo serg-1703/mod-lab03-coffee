bcd617f7ded6936358644e86b470cdbdc510ec9f
serg-1703 <vlasov0210serg@gmail.com>
2025-04-06 19:21:47 +0300

    i hate this if

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -80,7 +80,8 @@ void Automata::cook() {
     auto printing_state = std::chrono::seconds(2);
     state = COOK;
     while (std::chrono::steady_clock::now() - start < duration) {
-        if (std::chrono::steady_clock::now() - start == printing_state) getState();
+        if (std::chrono::steady_clock::now() - start == printing_state) 
+            getState();
     }
     std::cout << "Drink prepapred!\n";
     finish();
